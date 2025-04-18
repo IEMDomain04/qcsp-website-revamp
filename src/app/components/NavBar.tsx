6bff1aa12e7fa2a8abfa6af1bd24186dc5f1568f
IEMDomain04 <emmanman0011@gmail.com>
2025-04-18 17:03:52 +0800

    Feature: Navigation Bar Implemented

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 
+
+// Navigation links
 const navLinks = [
     { name: 'Home', href: '/' },
     { name: 'Explore', href: '/explore' },
@@ -10,14 +12,21 @@ const navLinks = [
 const NavBar = () => {
     return (
         <div className='w-full flex justify-between items-center px-40'>
+
+
+            {/* Logo */}
             <Image src="/assets/QCSP.svg" alt="Logo" width={400} height={300} />
 
+
+            {/* Navigation Links */}
             <div className='flex space-x-20'>
                 {navLinks.map((link) => (
                     <a key={link.name} href={link.href} className="border-b border-transparent hover:border-yellow-400 duration-300 hover:text-yellow-400">
                         {link.name}
                     </a>
                 ))}
+
+
             </div>
         </div>
     )
