ffa9ab829454ea995f730ba9b906218186438793
IEMDomain04.dc <emmanman0011@gmail.com>
2025-05-04 00:03:08 +0800

    Added Quantum Background to Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Nunito } from "next/font/google"; // ⬅️ Add Nunito here
 import "./globals.css";
+import QuantumBackground from "@/app/components/quantum-background"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${nunito.variable} antialiased`}
       >
+        <QuantumBackground />
         {children}
       </body>
     </html>
